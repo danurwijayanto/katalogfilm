87863a42b6e4f2c83b9dfcb5db0d78009a4588c1
danurwijayanto <mobinity.fx@gmail.com>
2019-12-08 19:07:41 +0700

    Implements in Main Activity

--- app/src/main/java/com/example/katalogfilm/MainActivity.java
+++ app/src/main/java/com/example/katalogfilm/MainActivity.java
@@ -2,21 +2,52 @@ package com.example.katalogfilm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    private String[] dataName = {"Cut Nyak Dien","Ki Hajar Dewantara","Moh Yamin","Patimura","R A Kartini","Sukarno"};
+//    private String[] dataName = {"Cut Nyak Dien","Ki Hajar Dewantara","Moh Yamin","Patimura","R A Kartini","Sukarno"};
+    private FilmAdapter adapter;
+    private String[] dataName;
+    private String[] dataDescription;
+    private TypedArray dataPhoto;
+    private ArrayList<Film> films;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ListView listView = findViewById(R.id.lv_list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
+        adapter = new FilmAdapter(this);
         listView.setAdapter(adapter);
+
+        prepare();
+        addItem();
+    }
+
+    private void prepare() {
+        dataName = getResources().getStringArray(R.array.data_name);
+        dataDescription = getResources().getStringArray(R.array.data_description);
+        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+    }
+
+    private void addItem() {
+        films = new ArrayList<>();
+
+        for (int i = 0; i < dataName.length; i++) {
+            Film film = new Film();
+            film.setFilm(dataPhoto.getResourceId(i, -1));
+            film.setJudul(dataName[i]);
+            film.setDescription(dataDescription[i]);
+            films.add(film);
+        }
+        adapter.setFilms(films);
     }
 }
