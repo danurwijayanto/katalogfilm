f99d46bc925315a69c98090f130a375efccaaf42
danurwijayanto <mobinity.fx@gmail.com>
2019-12-09 11:38:03 +0700

    Add parcelable

---
+++ app/src/main/java/com/example/katalogfilm/FilmParcelable.java
@@ -0,0 +1,75 @@
+package com.example.katalogfilm;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class FilmParcelable implements Parcelable {
+    private int film;
+    private String judul;
+    private String description;
+    private String tanggalRilis;
+
+    public int getFilm() {
+        return film;
+    }
+
+    public void setFilm(int film) {
+        this.film = film;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTanggalRilis() {
+        return tanggalRilis;
+    }
+
+    public void setTanggalRilis(String tanggalRilis) {
+        this.tanggalRilis = tanggalRilis;
+    }
+
+    protected FilmParcelable(Parcel in) {
+        film = in.readInt();
+        judul = in.readString();
+        description = in.readString();
+        tanggalRilis = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(film);
+        dest.writeString(judul);
+        dest.writeString(description);
+        dest.writeString(tanggalRilis);
+    }
+
+    public static final Creator<FilmParcelable> CREATOR = new Creator<FilmParcelable>() {
+        @Override
+        public FilmParcelable createFromParcel(Parcel source) {
+            return  new FilmParcelable(source);
+        }
+
+        @Override
+        public FilmParcelable[] newArray(int size) {
+            return new FilmParcelable[size];
+        }
+    };
+}
