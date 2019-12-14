034e3b39aac13ef4748912c22ce6550746ca2f69
danurwijayanto <mobinity.fx@gmail.com>
2019-12-15 01:09:55 +0700

    Finishing before upload submission 1

--- app/src/main/java/com/example/katalogfilm/FilmDetailsActivity.java
+++ app/src/main/java/com/example/katalogfilm/FilmDetailsActivity.java
@@ -15,8 +15,10 @@ public class FilmDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.film_details);
 
-        TextView detailsFilmDescription = findViewById(R.id.tv_description_film);
-        ImageView detailsFilmImage = findViewById(R.id.tv_film_image);
+        TextView detailsFilmDescription = findViewById(R.id.film_description_details);
+        TextView detailsRelease = findViewById(R.id.film_rilis_details);
+        TextView detailsJudul = findViewById(R.id.film_judul_details);
+        ImageView detailsFilmImage = findViewById(R.id.film_image_details);
 
         FilmParcelable filmData = getIntent().getParcelableExtra(EXTRA_FILM);
 
@@ -25,10 +27,14 @@ public class FilmDetailsActivity extends AppCompatActivity {
         Integer imageFilm = filmData.getFilm();
         String rilisFilm = filmData.getTanggalRilis();
 
-        String descFull = "Judul : " + judulFilm + "\n\nTahun Keluar : " + rilisFilm + "\n\nDescripsi : " + descFilm;
+//        String descFull = "Judul : " + judulFilm + "\n\nTahun Keluar : " + rilisFilm + "\n\nDescripsi : " + descFilm;
 
-        detailsFilmDescription.setText(descFull);
+//        detailsFilmDescription.setText(descFull);
+        detailsFilmDescription.setText(descFilm);
+        detailsRelease.setText(rilisFilm);
+        detailsJudul.setText(judulFilm);
         detailsFilmImage.setImageResource(imageFilm);
+
 
 //        Log.d("myTag", desc);
     }
