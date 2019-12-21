ea82994b5cddb47d8ba97c1835e16865b5362546
danurwijayanto <mobinity.fx@gmail.com>
2019-12-21 13:53:18 +0700

    Deploy Recycle View

--- app/src/main/java/com/example/katalogfilm/FilmAdapterRecycle.java
+++ app/src/main/java/com/example/katalogfilm/FilmAdapterRecycle.java
@@ -1,4 +1,60 @@
 package com.example.katalogfilm;
 
-public class FilmAdapterRecycle {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class FilmAdapterRecycle extends RecyclerView.Adapter<FilmAdapterRecycle.ListViewHolder>{
+    private ArrayList<FilmParcelable> listFilm;
+
+    public FilmAdapterRecycle(ArrayList<FilmParcelable> list) {
+        this.listFilm = list;
+    }
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_new_movie, parent, false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        FilmParcelable film = listFilm.get(position);
+
+        Glide.with(holder.itemView.getContext())
+                .load(film.getFilm())
+                .apply(new RequestOptions().override(55, 55))
+                .into(holder.imgPhoto);
+        holder.filmJudul.setText(film.getJudul());
+        holder.filmDescription.setText(film.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listFilm.size();
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+        TextView filmJudul, filmDescription;
+
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgPhoto = itemView.findViewById(R.id.img_item_photo);
+            filmJudul = itemView.findViewById(R.id.film_item_name);
+            filmDescription = itemView.findViewById(R.id.film_item_description);
+        }
+    }
 }
--- app/src/main/java/com/example/katalogfilm/MainActivity.java
+++ app/src/main/java/com/example/katalogfilm/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.katalogfilm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,67 +18,66 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private String[] dataName = {"Cut Nyak Dien","Ki Hajar Dewantara","Moh Yamin","Patimura","R A Kartini","Sukarno"};
-    private FilmAdapter adapter;
-    private String[] dataName;
-    private String[] dataDescription;
-    private String[] dataRilis;
-    private TypedArray dataPhoto;
-    private ArrayList<FilmParcelable> filmss;
+    private RecyclerView filmRecycle;
+    private ArrayList<FilmParcelable> list = new ArrayList<>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_recycle);
 
-        ListView listView = findViewById(R.id.lv_list);
-        adapter = new FilmAdapter(this);
-        listView.setAdapter(adapter);
+        filmRecycle = findViewById(R.id.movie_list);
+        filmRecycle.setHasFixedSize(true);
 
-        prepare();
-        addItem();
+        list.addAll(getListFilms());
+        showRecyclerList();
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
-                FilmParcelable filmItemParcel = new FilmParcelable();
-                filmItemParcel.setFilm(filmss.get(i).getFilm());
-                filmItemParcel.setJudul(filmss.get(i).getJudul());
-                filmItemParcel.setDescription(filmss.get(i).getDescription());
-                filmItemParcel.setTanggalRilis(filmss.get(i).getTanggalRilis());
-////
-                Intent filmDetailsActivity = new Intent(MainActivity.this, FilmDetailsActivity.class);
-                filmDetailsActivity.putExtra(FilmDetailsActivity.EXTRA_FILM, filmItemParcel);
-                startActivity(filmDetailsActivity);
-//                Toast.makeText(MainActivity.this, filmss.get(i).getJudul(), Toast.LENGTH_SHORT).show();
-            }
-        });
-//        break;
+//        ListView listView = findViewById(R.id.lv_list);
+//        adapter = new FilmAdapter(this);
+//        listView.setAdapter(adapter);
+//
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+//                FilmParcelable filmItemParcel = new FilmParcelable();
+//                filmItemParcel.setFilm(filmss.get(i).getFilm());
+//                filmItemParcel.setJudul(filmss.get(i).getJudul());
+//                filmItemParcel.setDescription(filmss.get(i).getDescription());
+//                filmItemParcel.setTanggalRilis(filmss.get(i).getTanggalRilis());
+//////
+//                Intent filmDetailsActivity = new Intent(MainActivity.this, FilmDetailsActivity.class);
+//                filmDetailsActivity.putExtra(FilmDetailsActivity.EXTRA_FILM, filmItemParcel);
+//                startActivity(filmDetailsActivity);
+////                Toast.makeText(MainActivity.this, filmss.get(i).getJudul(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+////        break;
     }
 
-    private void prepare() {
-        dataName = getResources().getStringArray(R.array.data_name);
-        dataDescription = getResources().getStringArray(R.array.data_description);
-        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
-        dataRilis = getResources().getStringArray(R.array.data_rilis);
-    }
 
-    private void addItem() {
-        filmss = new ArrayList<>();
+    public ArrayList<FilmParcelable> getListFilms() {
+        String[] dataName = getResources().getStringArray(R.array.data_name);
+        String[] dataDescription = getResources().getStringArray(R.array.data_description);
+        TypedArray dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+        String[] dataRilis = getResources().getStringArray(R.array.data_rilis);
 
+        ArrayList<FilmParcelable> listFilm = new ArrayList<>();
         for (int i = 0; i < dataName.length; i++) {
-            FilmParcelable filmParcel = new FilmParcelable();
-            filmParcel.setFilm(dataPhoto.getResourceId(i, -1));
-            filmParcel.setJudul(dataName[i]);
-            filmParcel.setDescription(dataDescription[i]);
-            filmParcel.setTanggalRilis(dataRilis[i]);
+            FilmParcelable film = new FilmParcelable();
+            film.setFilm(dataPhoto.getResourceId(i, -1));
+            film.setJudul(dataName[i]);
+            film.setDescription(dataDescription[i]);
+            film.setTanggalRilis(dataRilis[i]);
 
-//            film.setFilm(dataPhoto.getResourceId(i, -1));
-//            film.setJudul(dataName[i]);
-//            film.setDescription(dataDescription[i]);
-            filmss.add(filmParcel);
+            listFilm.add(film);
         }
-        adapter.setFilms(filmss);
+        return listFilm;
+    }
+
+    private void showRecyclerList() {
+        filmRecycle.setLayoutManager(new LinearLayoutManager(this));
+        FilmAdapterRecycle filmAdapterRecycle = new FilmAdapterRecycle(list);
+        filmRecycle.setAdapter(filmAdapterRecycle);
     }
 }
