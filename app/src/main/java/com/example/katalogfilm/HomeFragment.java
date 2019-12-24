90c8ae579055bd8a09dd297472c532ab734414d2
danurwijayanto <mobinity.fx@gmail.com>
2019-12-25 06:23:37 +0700

    Add data to fragment

---
+++ app/src/main/java/com/example/katalogfilm/HomeFragment.java
@@ -0,0 +1,100 @@
+package com.example.katalogfilm;
+
+
+import android.content.res.TypedArray;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    private RecyclerView filmRecycle;
+    private ArrayList<FilmParcelable> list = new ArrayList<>();
+
+    public static HomeFragment newInstance(int index) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated (View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final TextView textView = view.findViewById(R.id.section_label);
+
+        int index = 1;
+        if (getArguments() != null) {
+            index  = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+
+        switch (index) {
+            case 1:
+                filmRecycle = view.findViewById(R.id.movie_list);
+                filmRecycle.setHasFixedSize(true);
+
+                list.addAll(getListFilms());
+                showRecyclerList();
+                break;
+            case 2:
+                break;
+        }
+
+//        textView.setText(getString(R.string.content_tab_text) + " " + index);
+    }
+
+    public ArrayList<FilmParcelable> getListFilms() {
+        String[] dataName = getResources().getStringArray(R.array.data_name);
+        String[] dataDescription = getResources().getStringArray(R.array.data_description);
+        TypedArray dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+        String[] dataRilis = getResources().getStringArray(R.array.data_rilis);
+
+        ArrayList<FilmParcelable> listFilm = new ArrayList<>();
+        for (int i = 0; i < dataName.length; i++) {
+            FilmParcelable film = new FilmParcelable();
+            film.setFilm(dataPhoto.getResourceId(i, -1));
+            film.setJudul(dataName[i]);
+            film.setDescription(dataDescription[i]);
+            film.setTanggalRilis(dataRilis[i]);
+
+            listFilm.add(film);
+        }
+        return listFilm;
+    }
+
+    private void showRecyclerList() {
+        filmRecycle.setLayoutManager(new LinearLayoutManager(getActivity()));
+        FilmAdapterRecycle filmAdapterRecycle = new FilmAdapterRecycle(list);
+        filmRecycle.setAdapter(filmAdapterRecycle);
+    }
+
+}
--- app/src/main/java/com/example/katalogfilm/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/katalogfilm/SectionsPagerAdapter.java
@@ -26,15 +26,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        Fragment fragment = null;
-        switch (position) {
-            case 0:
-                fragment = new FilmFragment();
-                break;
-            case 1:
-                fragment = new TvFragment();
-                break;
-        }
+        Fragment fragment = HomeFragment.newInstance(position + 1);
         return fragment;
     }
 
