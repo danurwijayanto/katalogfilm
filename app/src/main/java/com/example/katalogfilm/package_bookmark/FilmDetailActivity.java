70a8baef138a12d5b01e0e109f47b5375c57332b
danurwijayanto <mobinity.fx@gmail.com>
2020-01-25 14:59:47 +0700

    Create activity

---
+++ app/src/main/java/com/example/katalogfilm/package_bookmark/FilmDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.katalogfilm.package_bookmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.katalogfilm.R;
+
+public class FilmDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film_details);
+    }
+}
